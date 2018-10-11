b1689bc0b43e0c11c1c8927de02565e187c89ef8
Yeqing Liu <chingliu2@gmail.com>
2018-10-11 13:34:39 -0400

    added other features to developed seperately

---
+++ index.js
@@ -0,0 +1,16 @@
+//Weather API (climacell)
+const CLIMA_BASE_URL = 'https://api2.climacell.co/v2';
+
+const climaKey //add api key here (use on 'apikey' header parameter)
+
+//Geocoding API (LocationIQ)
+
+const GEO_BASE_URL = 'https://us1.locationiq.com/v1/search.php'
+
+const geocodeKEY = `1a0665710b35a7`;
+
+//TODO: convert user input into geocode (longitude and latitude WGS 84). Need the google geocode API
+
+//Currency API
+const XCHANGE_BASE_URL = 'https://api.exchangeratesapi.io/latest';
+//need to add "?base=USD" to endpoint URL
